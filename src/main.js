0f94793bcfdae16c2924d5917292d03b1539153b
Liêm Đặng <hieuliem33@gmail.com>
2021-11-27 22:01:55 +0700

    change app. listen

--- src/main.js
+++ src/main.js
@@ -12,6 +12,9 @@ app.use(express.static(path.join(__dirname, "web")));
 
 app.use("/api", router);
 
-app.listen(port, () => {
-    console.log("http://localhost:" + port);
+// app.listen(port, () => {
+//     console.log("http://localhost:" + port);
+// });
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
